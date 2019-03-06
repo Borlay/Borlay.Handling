9185f598ed10d79d8cff6a1c7187230df56aed0f
Loat <pauliukss@gmail.com>
2019-03-06 19:09:48 +0200

    nuget changes

---
+++ Borlay.Handling/Borlay.Handling.Tests/HandlingTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Borlay.Handling.Tests
+{
+    [TestClass]
+    public class HandlingTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
