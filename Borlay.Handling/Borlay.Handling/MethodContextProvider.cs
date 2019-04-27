e9745075942bf1966fc1f7466d53c7f1fc10eb2d
Loat <pauliukss@gmail.com>
2019-04-27 19:17:47 +0300

    renamed interface

--- Borlay.Handling/Borlay.Handling/MethodContextProvider.cs
+++ Borlay.Handling/Borlay.Handling/MethodContextProvider.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Borlay.Handling
 {
-    public interface ITypeContextProvider
+    public interface IMethodContextProvider
     {
         TypeContext GetMethodContext(Type type);
     }
@@ -22,7 +22,7 @@ namespace Borlay.Handling
         MethodContextInfo[] GetMethodContextInfo(Type type);
     }
 
-    public class MethodContextProvider : ITypeContextProvider, IMethodContextInfoProvider
+    public class MethodContextProvider : IMethodContextProvider, IMethodContextInfoProvider
     {
         protected readonly ConcurrentDictionary<Type, TypeContext> contexts = new ConcurrentDictionary<Type, TypeContext>();
 
