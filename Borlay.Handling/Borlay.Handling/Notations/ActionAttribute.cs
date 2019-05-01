4174939fb6ffc24c589f013c654a486e07ae7a75
Loat <pauliukss@gmail.com>
2019-05-01 15:24:45 +0300

    Action and Scope attributes constructor

--- Borlay.Handling/Borlay.Handling/Notations/ActionAttribute.cs
+++ Borlay.Handling/Borlay.Handling/Notations/ActionAttribute.cs
@@ -9,8 +9,6 @@ namespace Borlay.Handling.Notations
     [AttributeUsage(AttributeTargets.Method)]
     public class ActionAttribute : Attribute, IGetId
     {
-        private readonly byte[] nameBytes;
-
         public string MethodName { get; }
 
         public ActionAttribute([CallerMemberName] string methodName = "")
@@ -19,10 +17,11 @@ namespace Borlay.Handling.Notations
                 throw new ArgumentNullException(nameof(methodName));
 
             this.MethodName = methodName.ToLower();
+        }
 
-            nameBytes = Encoding.UTF8.GetBytes(this.MethodName);
-            if (nameBytes.Length > Byte.MaxValue)
-                throw new ArgumentException("Method name is too long");
+        public ActionAttribute(int id)
+        {
+            this.MethodName = id.ToString();
         }
 
         public string GetId()
--- Borlay.Handling/Borlay.Handling/Notations/ScopeAttribute.cs
+++ Borlay.Handling/Borlay.Handling/Notations/ScopeAttribute.cs
@@ -8,8 +8,6 @@ namespace Borlay.Handling.Notations
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface)]
     public class ScopeAttribute : Attribute, IGetId
     {
-        private readonly byte[] nameBytes;
-
         public string ScopeName { get; }
 
         public ScopeAttribute(string scopeName)
@@ -18,10 +16,11 @@ namespace Borlay.Handling.Notations
                 throw new ArgumentNullException(nameof(scopeName));
 
             this.ScopeName = scopeName.ToLower();
+        }
 
-            nameBytes = Encoding.UTF8.GetBytes(this.ScopeName);
-            if (nameBytes.Length > Byte.MaxValue)
-                throw new ArgumentException("Method name is too long");
+        public ScopeAttribute(int id)
+        {
+            this.ScopeName = id.ToString();
         }
 
         public string GetId()
